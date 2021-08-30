e8bc19e83547ee508708f5e330770b11a0499787
Rushikesh Bhattad <rushikeshbhattad8@gmail.com>
2021-08-30 15:37:35 +0530

    Add project files.

---
+++ ConsoleAppStruct/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DemoStruct
+{
+    public struct Person
+    {
+        public String name;
+        public int Age;
+        public int weight;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1;
+            p1.name = "Rushi";
+            p1.Age = 22;
+            p1.weight = 62;
+            Console.WriteLine("The Details of Person Is Name = " + p1.name + " Age = " + p1.Age + " Weigtht = " + p1.weight);
+            Console.ReadKey();
+        }
+    }
+}
